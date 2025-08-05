90e717a9088c1566518325d2be506bd68426d5f6
Ny <ralaivaonylalaina@gmail.com>
2025-08-05 13:22:32 +0300

    debeug

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 function Hero() {
     return (
         <>
-            <section className="relative bg-[url('src/assets/images/hero.jpeg')] bg-cover bg-center h-[70vh] text-white">
+            <section className="relative bg-[url('hero.jpeg')] bg-cover bg-center h-[70vh] text-white">
                 <div className="absolute inset-0 bg-black/50"></div>
                 <div className="relative z-20 p-20">
                     <div className="max-w-xl">
